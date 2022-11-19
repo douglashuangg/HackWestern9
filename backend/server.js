0c2cb68cc0af1ee75e4edb264c8213ffe8018e69
SemiCollin <ckwan26.email@gmail.com>
2022-11-19 17:51:37 -0500

    DB connection issue

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,49 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const User = require("C:/HackWestern9/models/user")
 
-// app.get("/", function (req, res) {
-//   res.render("index", {});
-// });
+
+const uri = "mongodb+srv://jeffrey:<03120312Jeff!>@cluster0.emywofb.mongodb.net/Users?retryWrites=true&w=majority"; //
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+    // .then(() => console.log('connected'))
+    // .catch((err) => console.log(err));
+const db = mongoose.connection
+db.on("error" , console.error.bind(console, "MongoDB connection error:"));
+db.once("open" , () => {
+    console.log("db connected");
+})
+db.collection('Users')
+
+app.set("view engine" , "ejs");
+
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/login" , function(req, res) {
+    res.render("user");
+});
+
+app.post("/login", function (req, res) {
+    console.log(req.body.username);
+  const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+  });
+  user.save(function (err) {
+      if (err) {
+          throw err;
+      } else {
+        res.render("user");
+      }
+  });
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "ligma balls" });
@@ -17,13 +56,3 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-// app.use(cors());
-// app.usehttp://localhost:3001/api(express.json());
-// server accepts json in body of request, server can read json
-
-// app.use("/api/v1/userinfo", userinfo);
-// app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
-// returns 404 page if the req was not found
-
-// export default app;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,6 +4,11 @@ export default function Login() {
   return (
     <div>
       <h1>Login</h1>
+      <form action="/login" method="post">
+        <input type="text" placeholder="Username" name="username"/>
+        <input type="text" placeholder="Password" name="password"/>
+        <button type="submit">Submit</button>
+    </form>
     </div>
   );
 }
