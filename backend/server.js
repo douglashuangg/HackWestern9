fef6ff81843dc9f6854b89426c88eae7a1a453a7
SemiCollin <ckwan26.email@gmail.com>
2022-11-19 12:24:24 -0500

    basic backend set up

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
-import express from "express";
-import cors from "cors";
-import userinfo from "./api/userinfo.route.js";
+// import express from "express";
+// import cors from "cors";
+// import userinfo from "./api/userinfo.route.js";
 
+const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.get("/api" , (req , res) => {
+    res.json({ message: "ligma balls" });
+})
+
+app.listen(PORT , () => {
+    console.log(`Server listening on ${PORT}`);
+});
+
+// app.use(cors());
+// app.use(express.json());
 // server accepts json in body of request, server can read json
 
-app.use("/api/v1/userinfo", userinfo);
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+// app.use("/api/v1/userinfo", userinfo);
+// app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 // returns 404 page if the req was not found
 
-export default app;
+// export default app;
