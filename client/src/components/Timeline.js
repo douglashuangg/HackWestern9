2b70a9a2cf7b62fc432b4ffffc71c2988f17ba35
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2022-11-20 04:01:17 -0500

    get posts from db

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -2,35 +2,44 @@ import React, { useState } from "react";
 
 function Timeline() {
   const [data, setData] = useState({});
+  const [goal, setGoal] = useState({});
+  const [receivedPosts, setReceivedPosts] = useState([]);
+
   function updatePost(event) {
-    setData({ post: event.target.value });
+    setData(event.target.value);
+  }
+  function updateGoal(event) {
+    setGoal(event.target.value);
+  }
+
+  function getAllUserPosts() {
+    fetch("/GetAllUserPosts")
+      .then((res) => res.json())
+      .then((data) => {
+        setReceivedPosts((prevState) => [...prevState, data.message]);
+        console.log(data);
+      });
   }
 
   function savePost() {
+    const allData = [{ posts: data, goal: goal }];
+    console.log(allData);
     fetch("/yourtimeline", {
       method: "POST", // or 'PUT',
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(allData),
     })
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", data);
+        getAllUserPosts();
       })
       .catch((error) => {
         console.error("Error:", error);
       });
-    // const requestOptions = {
-    //   method: "POST",
-    //   mode: "cors",
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify({ title: "React POST Request Example" }),
-    // };
-    // fetch("/yourtimeline", requestOptions)
-    //   .then((response) => response.json())
-    //   .then((data) => this.setState({ postId: data.id }));
   }
 
   return (
@@ -41,8 +50,20 @@ function Timeline() {
         placeholder="What did you learn?"
         onChange={updatePost}
       ></textarea>
+      <label>Goal</label>
+      <textarea
+        placeholder="What goal is this under (should be select options)"
+        onChange={updateGoal}
+      ></textarea>
       <button onClick={savePost}>Save</button>
       <p>{data.post}</p>
+      <p>{goal.goal}</p>
+      {receivedPosts.map((item) => (
+        <>
+          <h1>{item.posts}</h1>
+          <h2>{item.goal}</h2>
+        </>
+      ))}
     </div>
   );
 }
