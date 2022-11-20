a4e8dc280bc4cef2f68891523de6e875ecb51afb
Sung-En-Huang <s385huan@uwaterloo.ca>
2022-11-19 22:43:12 -0500

    Front End Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import React from "react";
 import Home from "./pages/home/Home";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 // import Login from "./components/Login";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 
 function App() {
-  return <Home />;
+  return <Profile />;
 }
 
 export default App;
--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,28 +1,41 @@
 import "./Topbar.css";
-import { Search, Person } from "@mui/icons-material";
+import { Search, Person, Feed, Notifications } from "@mui/icons-material";
+
 export default function Topbar() {
   return (
     <div className="topbarContainer">
-      <div className="topbarLeft"></div>
-      <span className="logo">TopG</span>
-      <div className="topbarCenter"></div>
-      <div className="searchbar"></div>
-      <Search />
-      <input
-        placeholder="Search for friends, goals, or photos"
-        className="searchInput"
-      />
-
-      <div className="topbarRight"></div>
-      <div className="topbarlinks">
-        <span className="topbarLink">Homepage</span>
-        <span className="topbarLink">Timeline</span>
+      <div className="topbarLeft">
+        <span className="logo">TopG</span>
       </div>
-      <div className="topbarIcons">
-        <div className="topbarIconItem">
-          <Person />
-          <span className="topbarIconBadge">1</span>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
         </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink"></span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Feed />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/Pfp/Pfp1.jpg" alt="" className="topbarImg" />
       </div>
     </div>
   );
