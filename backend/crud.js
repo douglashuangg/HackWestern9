edfc7e74ef2bd97286e0c35cd733077f909cdc9d
SemiCollin <ckwan26.email@gmail.com>
2022-11-20 02:37:55 -0500

    CRUD implemented

--- backend/crud.js
+++ backend/crud.js
@@ -6,24 +6,75 @@ async function main() {
 
   const client = new MongoClient(uri);
 
-  try {
-    await client.connect();
-    await findOneListingByName(client);
-  } finally {
-    await client.close();
-  }
+  // try {
+  //   await client.connect();
+  //   await read(client , "dhuang");
+  // } finally {
+  //   await client.close();
+  // }
 }
 main().catch(console.error);
 
-async function findOneListingByName(client) {
+async function create(client , firstname , username , password) {
   const result = await client
-    .db("HACKWESTERN")
-    .collection("Users")
-    .findOne({ firstname: "Jeffrey" });
+  .db("HACKWESTERN")
+  .collection("Users")
+  .insertOne({
+    firstname: firstname,
+    username: username,
+    password: password,
+  })
+
+  if (result) {
+    console.log(result);
+    validInput = true;
+  } else {
+    console.log(`Error Creating User`);
+  }
+}
+
+async function read(client , username) {
+  const result = await client
+  .db("HACKWESTERN")
+  .collection("Users")
+  .findOne({ 
+    username: username
+  });
 
   if (result) {
     console.log(result);
   } else {
-    console.log(`No listings found with the name`);
+    console.log(`No Listings Found With that Name`);
+  }
+}
+
+async function update(client , username , goal) {
+  const result = await client
+  .db("HACKWESTERN")
+  .collection("Users")
+  .updateOne(
+    {username: username},
+    {$set: {goal: goal}}
+  )
+
+  if (result) {
+    console.log(result);
+  } else {
+    console.log(`Error Updating Goal`)
+  }
+}
+
+async function destroy(client , username) {
+  const result = await client
+  .db("HACKWESTERN")
+  .collection("Users")
+  .deleteOne(
+    {username: username}
+  )
+
+  if (result) {
+    console.log(result);
+  } else {
+    console.log(`Error Deleting User`)
   }
 }
